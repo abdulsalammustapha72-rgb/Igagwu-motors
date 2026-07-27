39b87ddf14f171a25723d9566ec8a170268364db
Abdulsalam mustapha <abdulsalammustapha72@icloud.com>
2026-07-27 18:46:14 +0100

    Update error message in ForgotPassword component

--- src/auth/ForgotPassword.jsx
+++ src/auth/ForgotPassword.jsx
@@ -28,7 +28,7 @@ const ForgotPassword = () => {
             }, 1500);
 
         } catch (err) {
-            setErrorMessage(err.response?.data?.message || err.message);
+            setErrorMessage('Something went wrong, Please try again.');
         };
     };
 
