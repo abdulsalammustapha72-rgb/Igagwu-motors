74c5411a2c6381aa159189dd6bb0dc5d5cd198d1
Abdulsalam Mustapha <Abdulsalammustapha72@gmail.com>
2026-07-27 16:18:14 +0100

    Update backend server port

--- Backend/Server.js
+++ Backend/Server.js
@@ -64,6 +64,8 @@ app.use('/api', authRoute);
 
 ConnectDB();
 
-app.listen(5000, () => {
-    console.log('Server is running on PORT 5000')
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 });
